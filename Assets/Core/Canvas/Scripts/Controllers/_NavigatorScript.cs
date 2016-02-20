8d914c4533210e3fc8a2d421f9cd69667a7cec24
dh624 <dh624@scarletmail.rutgers.edu>
2016-02-20 14:52:27 -0500

    working on animator

--- Assets/Core/Canvas/Scripts/Controllers/_NavigatorScript.cs
+++ Assets/Core/Canvas/Scripts/Controllers/_NavigatorScript.cs
@@ -6,9 +6,15 @@ public class _NavigatorScript : MonoBehaviour
 {	
 	private NavMeshAgent agent;
 	private Animator animator;
+    public GameObject gameBall;
 	private float angleDiff;
+    public bool IsInitialized;
+    public bool HasHandshaked;
+    public bool Tackle;
+    public bool IsHoldingBall;
 
-	[HideInInspector]
+
+    [HideInInspector]
 	public Quaternion desiredOrientation{ get; set; }
 
 	/*put together with locomotion*/
@@ -19,35 +25,37 @@ public class _NavigatorScript : MonoBehaviour
 
 	public void Initialize() 
 	{
+        gameBall = this.GetComponent<GameObject>();
 		agent = this.GetComponent<NavMeshAgent> ();
 		animator = this.GetComponent<Animator> ();
         desiredOrientation = transform.rotation;
-
+        IsInitialized = false;
+        HasHandshaked = false;
 		/*put together with locomotion*/
 		locomotion = new LocomotionController(animator);
 
 	}
 
-	protected void SetupAgentLocomotion()
-	{
-		if (AgentDone())
-		{
+    protected void SetupAgentLocomotion()
+    {
+        if (AgentDone())
+        {
             //TODO Resetting path here, otherwise e.g. stepback animation not working properly - CS 03.09.2014
             //Is there a better solution?
             agent.ResetPath();
-			locomotion.Do(0, angleDiff);
-		}
-		else
-		{
-			float speed = agent.desiredVelocity.magnitude;
-			
-			Vector3 velocity = Quaternion.Inverse(transform.rotation) * agent.desiredVelocity;
-			
-			float angle = Mathf.Atan2(velocity.x, velocity.z) * 180.0f / 3.14159f;
-			
-			locomotion.Do(speed, angle);
-		}
-	}
+            locomotion.Do(0, angleDiff);
+        }
+        else
+        {
+            float speed = agent.desiredVelocity.magnitude;
+
+            Vector3 velocity = Quaternion.Inverse(gameBall.transform.rotation) * agent.desiredVelocity;
+
+            float angle = Mathf.Atan2(velocity.x, velocity.z) * 180.0f / 3.14159f;
+
+            locomotion.Do(speed, angle);
+        }
+    }
 
     void OnAnimatorMove()
     {
@@ -67,16 +75,78 @@ public class _NavigatorScript : MonoBehaviour
 	{
 		return !agent.pathPending && AgentStopping();
 	}
-	
-	protected bool AgentStopping()
-	{
-		return agent.remainingDistance <= agent.stoppingDistance;
-	}
 
+    protected bool AgentStopping()
+    {
+        return agent.remainingDistance <= agent.stoppingDistance;
+    }
 	// Update is called once per frame
 	void Update () 
 	{
-		SetupAgentLocomotion();
+        SetupAgentLocomotion();
+        if(IsInitialized == false)
+        {
+            IsInitialized = true;
+            agent.SetDestination(new Vector3(0,0,0));
+            if(agent.transform.position == agent.destination)
+            {
+                IsInitialized = true;
+            }
+            return;
+        }
+        if(HasHandshaked == false)
+        {
+            if(true)
+            {
+                //code here for affordance
+                HasHandshaked = true;
+            }
+            return;          
+        }        
+        if(true/*gameBall.isPickedUp == false*/)
+        {
+            ResetBools();
+            agent.SetDestination(new Vector3(10f,0f,10f)/*gameBall.transform.position*/); //Ball will be picked up once destination is reached
+            if(agent.transform.position == agent.destination)
+            {
+                //pick up ball
+                /*gameBall.isPickedUp = true*/
+            }
+            return;
+        }
+        if(IsHoldingBall == true)
+        {
+            agent.SetDestination(new Vector3(10f, 0f, 10f)); // the goal
+            if(agent.transform.position == agent.destination)
+            {
+                animator.SetBool("B_Breakdance", true);
+                //end game
+                return;
+            }
+        }
+        else
+        {
+            agent.speed = 3.5f;
+            if(true)//whatever bool is checked to end game
+            {
+                //cry
+                //end game
+            }
+            Tackle = true;
+            if(agent.transform.position == agent.destination)
+            {
+                //tackle
+                /*gameBall.isPickedUp = false*/
+                return;
+            }
+        }        
 	}
+    void ResetBools()
+    {
+        IsHoldingBall = false;
+        Tackle = false;
+        agent.speed = 2.2f;
+        return;
+    }
 	
 }
